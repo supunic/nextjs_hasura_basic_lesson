0cd8747724938fdb6ad2726603c95776839ba7cc
seiya endo <seiya.endo0830@gmail.com>
2021-05-16 14:43:37 +0900

    fix: useQuery fetchPolicy

--- pages/hasura-main.tsx
+++ pages/hasura-main.tsx
@@ -7,7 +7,12 @@ import { Layout } from '../components/Layout'
 
 const FetchMain: VFC = () => {
   const { data, error, loading } = useQuery<GetUsersQuery>(GET_USERS, {
-    fetchPolicy: 'network-only'
+    fetchPolicy: 'cache-and-network', // 推奨 毎回サーバーサイドのデータを取得してキャッシュする（データ取得中は前キャッシュのデータを表示） → データ更新が多い or リアルタイムにしたい時
+    // fetchPolicy: 'network-only', // 毎回サーバーサイドのデータを取得してキャッシュする（データ取得中は何も表示されない） → データ更新が多い or リアルタイムにしたい時
+    // fetchPolicy: 'cache-first', // サーバーサイドの更新を見に行かない → データ更新が少ない場合
+    // fetchPolicy: 'no-cache', // キャッシュを使用しない → 通常のaxios spaの挙動
+    // fetchPolicy: 'cache-only',
+    // fetchPolicy: 'standby',
   })
   // useQueryはoption指定可能
   // デフォルトでキャッシュファースト
